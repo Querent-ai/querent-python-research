08d5a66cb50172ee937e12466392092637829eb2
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-08-13 19:36:35 -0500

    add some collector interfaces

---
+++ querent/collectors/collector_errors.py
@@ -0,0 +1,34 @@
+from typing import List, Dict
+from pathlib import Path
+from enum import Enum
+
+class CollectorErrorKind(Enum):
+    NotFound = "not_found"
+    Unauthorized = "unauthorized"
+    incompatible = "incompatible"
+    NotSupported = "not_supported"
+
+class CollectorResolverError(Exception):
+    def __init__(self, kind: CollectorErrorKind, message: str):
+        super().__init__(message)
+        self.kind = kind
+
+
+class CollectorError(Exception):
+    def __init__(self, kind: CollectorErrorKind, source: any):
+        self.kind = kind
+        self.source = source
+
+
+class NotFoundError(CollectorError):
+    def __init__(self, message: str):
+        super().__init__(CollectorErrorKind.NotFound, message)
+
+class UnauthorizedError(CollectorError):
+    def __init__(self, message: str):
+        super().__init__(CollectorErrorKind.unauthorized, message)
+
+
+class IncompatibleError(CollectorError):
+    def __init__(self, message: str):
+        super().__init__(CollectorErrorKind.incompatible, message)
---
+++ querent/config/collector_config.py
@@ -0,0 +1,43 @@
+from enum import Enum
+from typing import Optional
+from pydantic import BaseModel
+
+class CollectorBackend(str, Enum):
+    LocalFile = "localfile"
+    S3 = "s3"
+    Gcs = "gs"
+
+class CollectConfig(BaseModel):
+    backend: CollectorBackend
+    class Config:
+        use_enum_values = True
+
+class LocalFileCollectConfig(BaseModel):
+    root_path: str
+
+class S3CollectConfig(BaseModel):
+    bucket: str
+    region: str
+    access_key: str
+    secret_key: str
+
+class GcsCollectConfig(BaseModel):
+    bucket: str
+    region: str
+    access_key: str
+    secret_key: str
+
+class CollectConfigWrapper(BaseModel):
+    backend: CollectorBackend
+    config: Optional[BaseModel] = None
+
+    @classmethod
+    def from_collect_config(cls, collect_config: CollectConfig):
+        if collect_config.backend == CollectorBackend.LocalFile:
+            return cls(backend=CollectorBackend.LocalFile, config=LocalFileCollectConfig())
+        elif collect_config.backend == CollectorBackend.S3:
+            return cls(backend=CollectorBackend.S3, config=S3CollectConfig())
+        elif collect_config.backend == CollectorBackend.Gcs:
+            return cls(backend=CollectorBackend.Gcs, config=GcsCollectConfig())
+        else:
+            raise ValueError(f"Unsupported collector backend: {collect_config.backend}")
