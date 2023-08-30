8cd2553c7f03ebea4e3731246ea1992852872555
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-08-30 07:30:55 -0500

    working test

--- tests/test_pdf_ingestor.py
+++ tests/test_pdf_ingestor.py
@@ -6,22 +6,31 @@ from querent.common.uri import Uri
 from querent.ingestors.ingestor_manager import IngestorFactoryManager
 import pytest
 
+@pytest.mark.asyncio
 async def test_collect_and_ingest_pdf():
     # Set up the collector
     collector_factory = FSCollectorFactory()
     uri = Uri("file://" + str(Path("./tests/data/pdf/").resolve()))
     config = FSCollectorConfig(root_path=uri.path)
     collector = collector_factory.resolve(uri, config)
-
+    
     # Set up the ingestor
     ingestor_factory_manager = IngestorFactoryManager()
-    ingestor_factory = ingestor_factory_manager.get_factory("pdf")
+    ingestor_factory = await ingestor_factory_manager.get_factory("pdf")  # Notice the use of await here
     ingestor = await ingestor_factory.create("pdf", [])
-
+    
     # Collect and ingest the PDF
     ingested_call = ingestor.ingest(collector.poll())
-    async for ingested in ingested_call:
-        print(ingested)
+    counter = 0
+    async def poll_and_print():
+        counter = 0
+        async for ingested in ingested_call:
+            assert ingested is not None
+            if len(ingested)  == 0:
+                counter += 1
+        assert counter == 1
+    
+    await poll_and_print()  # Notice the use of await here
 
 
 if __name__ == "__main__":
