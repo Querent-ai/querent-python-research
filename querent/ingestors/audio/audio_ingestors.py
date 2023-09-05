dc9f18264bb09221d5d6ead1a94a4387dc2e15b3
Ansh5461 <anshjoshi0607@gmail.com>
2023-09-06 01:35:32 +0530

    Added audio ingestor

---
+++ querent/ingestors/audio/audio_ingestors.py
@@ -0,0 +1,101 @@
+from pydub import AudioSegment
+import io
+import speech_recognition as sr
+
+from typing import List, AsyncGenerator
+from querent.ingestors.ingestor_factory import IngestorFactory
+from querent.processors.async_processor import AsyncProcessor
+from querent.ingestors.base_ingestor import BaseIngestor
+from querent.config.ingestor_config import IngestorBackend
+from querent.common.types.collected_bytes import CollectedBytes
+
+
+class AudioIngestorFactory(IngestorFactory):
+    SUPPORTED_EXTENSIONS = {"mp3"}
+
+    async def supports(self, file_extension: str) -> bool:
+        return file_extension.lower() in self.SUPPORTED_EXTENSIONS
+
+    async def create(
+        self, file_extension: str, processors: List[AsyncProcessor]
+    ) -> BaseIngestor:
+        if not self.supports(file_extension):
+            return None
+        return AudioIngestor(processors)
+
+
+class AudioIngestor(BaseIngestor):
+    def __init__(self, processors: List[AsyncProcessor]):
+        super().__init__(IngestorBackend.MP3)
+        self.processors = processors
+
+    async def ingest(
+        self, poll_function: AsyncGenerator[CollectedBytes, None]
+    ) -> AsyncGenerator[str, None]:
+        current_file = None
+        collected_bytes = b""
+        try:
+            async for chunk_bytes in poll_function:
+                if chunk_bytes.is_error():
+                    # TODO handle error
+                    continue
+                if current_file is None:
+                    current_file = chunk_bytes.file
+                elif current_file != chunk_bytes.file:
+                    # we have a new file, process the old one
+                    async for text in self.extract_and_process_audio(
+                        CollectedBytes(file=current_file, data=collected_bytes)
+                    ):
+                        yield text
+                    collected_bytes = b""
+                    current_file = chunk_bytes.file
+                collected_bytes += chunk_bytes.data
+        except Exception as e:
+            # TODO handle exception
+            yield ""
+        finally:
+            # process the last file
+            async for text in self.extract_and_process_audio(
+                CollectedBytes(file=current_file, data=collected_bytes)
+            ):
+                yield text
+            pass
+
+    async def extract_and_process_audio(
+        self, collected_bytes: CollectedBytes
+    ) -> AsyncGenerator[str, None]:
+        text = await self.extract_text_from_audio(collected_bytes)
+        # print(text)
+        processed_text = await self.process_data(text)
+        yield processed_text
+
+    async def extract_text_from_audio(self, collected_bytes: CollectedBytes) -> str:
+        audio_segment = AudioSegment.from_file(
+            io.BytesIO(collected_bytes.data), format=collected_bytes.extension
+        )
+        temp_wave = io.BytesIO()
+        audio_segment.export(temp_wave, format="wav")
+        # Initialize the recognizer
+        recognizer = sr.Recognizer()
+
+        temp_wave.seek(0)
+        with sr.AudioFile(temp_wave) as source:
+            recognizer.adjust_for_ambient_noise(source, duration=1)
+            audio_data = recognizer.record(source)
+
+        # Recognize the text using the recognizer
+        try:
+            recognized_text = recognizer.recognize_google(audio_data, language="en-US")
+            return recognized_text
+        except sr.UnknownValueError:
+            print("Could not understand the audio")
+        except sr.RequestError as e:
+            print(f"Could not request results; {e}")
+        except Exception as e:
+            print(e)
+
+    async def process_data(self, text: str) -> List[str]:
+        processed_data = text
+        for processor in self.processors:
+            processed_data = await processor.process(processed_data)
+        return processed_data
