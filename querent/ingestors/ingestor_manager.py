0c96d174d1d695de204db937eb1d8a751fa19669
Ansh Joshi <54464396+Ansh5461@users.noreply.github.com>
2023-09-06 21:58:52 +0530

    Added json and images ingestor (#40)
    * Added json and images ingestor
    
    * Updated requirements.txt file
    
    * Updated dependencies
    
    * Updated dependencies
    
    * Updated dependencies of tesseract ocr
    
    * Updated requirements file

--- querent/ingestors/ingestor_manager.py
+++ querent/ingestors/ingestor_manager.py
@@ -1,26 +1,32 @@
-
 from typing import Optional
 from querent.config.ingestor_config import IngestorBackend
 from querent.ingestors.base_ingestor import BaseIngestor
 from querent.ingestors.ingestor_factory import IngestorFactory, UnsupportedIngestor
 from querent.ingestors.pdfs.pdf_ingestor_v1 import PdfIngestorFactory
+from querent.ingestors.json.json_ingestor import JsonIngestorFactory
+from querent.ingestors.images.image_ingestor import ImageIngestorFactory
 
 
 class IngestorFactoryManager:
     def __init__(self):
         self.ingestor_factories = {
             IngestorBackend.PDF.value: PdfIngestorFactory(),
-            #Ingestor.TEXT.value: TextIngestor(),
+            IngestorBackend.JSON.value: JsonIngestorFactory(),
+            IngestorBackend.JPG.value: ImageIngestorFactory(),
+            IngestorBackend.PNG.value: ImageIngestorFactory(),
+            # Ingestor.TEXT.value: TextIngestor(),
             # Add more mappings as needed
         }
 
     async def get_factory(self, file_extension: str) -> IngestorFactory:
-        return self.ingestor_factories.get(file_extension.lower(), UnsupportedIngestor("Unsupported file extension"))
+        return self.ingestor_factories.get(
+            file_extension.lower(), UnsupportedIngestor("Unsupported file extension")
+        )
 
     async def get_ingestor(self, file_extension: str) -> Optional[BaseIngestor]:
         factory = self.get_factory(file_extension)
         return factory.create(file_extension)
-    
+
     async def supports(self, file_extension: str) -> bool:
         factory = self.get_factory(file_extension)
         return factory.supports(file_extension)
