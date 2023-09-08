7b735548959a3340c413d8c2fdb3ff3c9ece1068
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-09-08 01:26:40 -0500

    fix test

--- tests/test_audio_ingestor.py
+++ tests/test_audio_ingestor.py
@@ -32,7 +32,7 @@ async def test_collect_and_ingest_audio():
             if len(ingested) == 0:
                 counter += 1
 
-    assert counter == 0
+    assert counter == 1
 
     await poll_and_print()
 
