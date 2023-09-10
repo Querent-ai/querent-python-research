2da8fc552eb4ee562e28ca5d98e62c91b253c060
Ansh Joshi <54464396+Ansh5461@users.noreply.github.com>
2023-09-10 09:47:19 +0530

    Added csv ingestor (#43)

--- querent/ingestors/ingestor_manager.py
+++ querent/ingestors/ingestor_manager.py
@@ -10,6 +10,7 @@ from querent.ingestors.texts.text_ingestor import TextIngestorFactory
 from querent.ingestors.audio.audio_ingestors import AudioIngestorFactory
 from querent.ingestors.json.json_ingestor import JsonIngestorFactory
 from querent.ingestors.images.image_ingestor import ImageIngestorFactory
+from querent.ingestors.csv.csv_ingestor import CsvIngestorFactory
 
 
 class IngestorFactoryManager:
@@ -24,6 +25,7 @@ class IngestorFactoryManager:
             IngestorBackend.JSON.value: JsonIngestorFactory(),
             IngestorBackend.JPG.value: ImageIngestorFactory(),
             IngestorBackend.PNG.value: ImageIngestorFactory(),
+            IngestorBackend.CSV.value: CsvIngestorFactory(),
             # Ingestor.TEXT.value: TextIngestor(),
             # Add more mappings as needed
         }
