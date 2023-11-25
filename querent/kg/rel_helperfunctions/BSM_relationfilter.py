79005776cdf8938ae17eaf5a910e3ed27283cd8e
ngupta10 <ngupta10.slb@gmail.com>
2023-11-25 20:24:23 +0530

    BSM_v1

--- querent/kg/rel_helperfunctions/BSM_relationfilter.py
+++ querent/kg/rel_helperfunctions/BSM_relationfilter.py
@@ -7,6 +7,10 @@ class BSMBranch:
         # Initialize the appropriate language model based on the provided model name
         self.llm_llama = CTransformers(model='./tests/llama-2-7b-chat.Q4_K_M.gguf', config=config, model_type='llama')
         self.llm_falcon = CTransformers(model='/home/nishantg/Downloads/falcon-7b-instruct.ggccv1.q4_1.bin', config=config, model_type='falcon')
+        self.sub_tasks = []
+    
+    def add_sub_task(self, task):
+        self.sub_tasks.append(task)
 
 
     def query_llm(self, llm, prompt, entity1, entity2, context):
@@ -18,17 +22,17 @@ class BSMBranch:
         extraction_prompt = "Given the entities {entity1} and {entity2} in the context: {context}, what is their relationship?"
         relationship = self.query_llm(self.llm_llama, extraction_prompt, entity1, entity2, context)
         print(relationship)
-        # Sub-tasks
-        sub_tasks = [
-            "Evaluate if a meaningful relationship exists between {entity1} and {entity2}, given their relationship is {context}.",
-            "Determine the type of relationship (e.g., location-based, person-based) between {entity1} and {entity2}, given their relationship is {context}.",
-            "Check the response for factual accuracy regarding the relationship between {entity1} and {entity2}, given their relationship is {context}.",
-            "Assess alignment with the user's question and relevance regarding the relationship between {entity1} and {entity2}, given their user's question is to extract the relationship.",
-            "Identify any temporal and causal relationships between {entity1} and {entity2}, given the relationship is {context}."
-        ]
+        if not self.sub_tasks:
+            self.sub_tasks = [
+                "Evaluate if a meaningful relationship exists between {entity1} and {entity2}, given their relationship is {context}.".strip(),
+                "Determine the type of relationship (e.g., location-based, person-based) between {entity1} and {entity2}, given their relationship is {context}.".strip(),
+                "Check the response for factual accuracy regarding the relationship between {entity1} and {entity2}, given their relationship is {context}.".strip(),
+                "Assess alignment with the user's question and relevance regarding the relationship between {entity1} and {entity2}, given their user's question is to extract the relationship.".strip(),
+                "Identify any temporal and causal relationships between {entity1} and {entity2}, given the relationship is {context}.".strip()
+            ]
 
         results = []
-        for task in sub_tasks:
+        for task in self.sub_tasks:
             prompt = task
             result_llama = self.query_llm(self.llm_llama, prompt, entity1, entity2, relationship)
             result_falcon = self.query_llm(self.llm_falcon, prompt, entity1, entity2, relationship)
@@ -37,8 +41,10 @@ class BSMBranch:
         return results
 
 # Example usage
-config = {'max_new_tokens': 50, 'temperature': 0.2, 'repetition_penalty': 1, 'context_length': 2000}
+config = {'max_new_tokens': 100, 'temperature': 0, 'repetition_penalty': 1.7, 'context_length': 2000}
 bsm_branch = BSMBranch(config=config)
+
+#bsm_branch.add_sub_task("Custom sub-task prompt with {entity1}, {entity2}, and {context}.")
 
 # Example entities and context
 entity1 = "Python"
