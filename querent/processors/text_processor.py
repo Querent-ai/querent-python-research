6031f6c99751d33c205f374886b57265ce235958
Ansh5461 <anshjoshi0607@gmail.com>
2023-12-25 18:52:49 +0530

    Added text processor

--- querent/processors/text_processor.py
+++ querent/processors/text_processor.py
@@ -27,15 +27,34 @@ class TextProcessor(AsyncProcessor):
                 while i < len(words_in_line):
                     word = words_in_line[i]
                     # Check if next word exists
-                    if i + 1 < len(words_in_line):
+                    if i + 1 < len(words_in_line) and i > 0:
                         next_word = words_in_line[i + 1]
-                        combined_word = word + next_word
+                        prev_word = words_in_line[i - 1]
+                        combined_next_word = word + next_word
+                        combined_prev_word = prev_word + word
                         # Join words only if the combined form is valid and neither of the individual words is valid
-                        if self.is_valid_word(combined_word) and not (
+                        if self.is_valid_word(combined_next_word) and not (
                             self.is_valid_word(word) or self.is_valid_word(next_word)
                         ):
-                            word = combined_word
+                            word = combined_next_word
                             i += 1
+                        elif self.is_valid_word(combined_prev_word) and not (
+                            self.is_valid_word(word) or self.is_valid_word(prev_word)
+                        ):
+                            # If individual word formed after joining current word and previous word is valid, then delete the previous word and replace it with new word
+
+                            word = combined_prev_word
+                            last_space_index = new_line.rfind(" ")
+                            if last_space_index != -1:
+                                second_last_space_index = new_line.rfind(
+                                    " ", 0, last_space_index
+                                )
+                                if second_last_space_index == -1:
+                                    second_last_space_index = 0
+                                new_line = (
+                                    new_line[:second_last_space_index]
+                                    + new_line[last_space_index:]
+                                )
                     new_line += word + " "
                     i += 1
 
@@ -47,7 +66,6 @@ class TextProcessor(AsyncProcessor):
                     # If the sentence ends with hyphen(-), then check if the word is incomplete, if yes then join them
                     current_word = words_in_line[len(words_in_line) - 2]
                     next_index = lines[index + 1].index(" ")
-                    print("next index    ", next_index)
                     next_word = lines[index + 1][:next_index]
                     new_word = current_word + next_word
                     if self.is_valid_word(new_word):
