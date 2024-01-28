cdc5ef8ba1c8eb5e554eeac222315df8d6d0dca6
Ansh Joshi <54464396+Ansh5461@users.noreply.github.com>
2024-01-28 19:14:48 +0530

    Added init in few folders (#226)

---
+++ querent/ingestors/__init__.py
@@ -0,0 +1,22 @@
+# Description: Querent is an async data dynamo that can be used to collect, ingest, process, and store data.
+#              It is designed to be used in a modular fashion, allowing for easy extensibility and customization.
+#              It is also designed for data intensive applications, and is built on top of asyncio
+#              to allow for high throughput and scalability.
+
+# Version: 0.1.0
+
+from .audio import *
+from .code import *
+from .csv import *
+from .doc import *
+from .email import *
+from .github import *
+from .html import *
+from .images import *
+from .json import *
+from .pdfs import *
+from .ppt import *
+from .texts import *
+from .video import *
+from .xlsx import *
+from .xml import *
