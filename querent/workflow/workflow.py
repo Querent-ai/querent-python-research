2e5fa070d3e7836945963e4c92f7f8a210d444c0
ngupta10 <ngupta10.slb@gmail.com>
2024-01-30 14:31:15 +0530

    workflow changes_2

--- querent/workflow/workflow.py
+++ querent/workflow/workflow.py
@@ -128,65 +128,58 @@ async def start_llama_workflow(config: Config):
 
 
 
-# async def main():
-#     class StateChangeCallback(EventCallbackInterface):
-#         def handle_event(self, event_type: EventType, event_state: EventState):
-#             assert event_state.event_type == EventType.Graph
-#             triple = json.loads(event_state.payload)
-#             print("triple: {}".format(triple))
-#             assert isinstance(triple['subject'], str) and triple['subject']
-#     config_source={
-#         "version": 1.0,
-#         "id": "ahdbfvd",
-#         "querent_id": 12345,
-#         "querent_name": "llama",
-#         "workflow": {
-#         "name": "llama",
-#         "id": str(uuid.uuid4()),
-#         "config": {},
-#         "event_handler": StateChangeCallback
-#     },
-#         "collectors": [{
-#             "id": str(uuid.uuid4()),
-#             "name": "Local-config",
-#             "config": {
-#                 "root_path": "./tests/data/llm/pdf",
-#             },
-#             "backend":"localfile",
-#         },
-#         {
-#             "id": str(uuid.uuid4()),
-#             "name": "Drive-config",
-#             "config": {
-#                 "drive_refresh_token": os.getenv("DRIVE_REFRESH_TOKEN"),
-#                 "drive_token": os.getenv("DRIVE_TOKEN"),
-#                 "drive_scopes": os.getenv("DRIVE_SCOPES"),
-#                 "chunk_size": 1024 * 1024,
-#                 "drive_client_id": os.getenv("DRIVE_CLIENT_ID"),
-#                 "drive_client_secret": os.getenv("DRIVE_CLIENT_SECRET"),
-#                 "specific_file_type": "application/pdf",
-#                 "folder_to_crawl": "1BtLKXcYBrS16CX0R4V1X7Y4XyO9Ct7f8",
-#             },
-#             "backend": "drive",
-#         }],
-        
-#         "engines": [{ 
-#         #https://github.com/Querent-ai/querent-rs/blob/main/src/config/config.rs#L172 con
-#         "id": str(uuid.uuid4()),
-#         "name": "knowledge_graph_using_llama2_v1",
-#         "config": {"ner_model_name":"botryan96/GeoBERT",
-#         "enable_filtering": True,
-#         "filter_params": {
-#                 'score_threshold': 0.5,
-#                 'attention_score_threshold': 0.1,
-#                 'similarity_threshold': 0.5,
-#                 'min_cluster_size': 5,
-#                 'min_samples': 3,
-#                 'cluster_persistence_threshold':0.1
-#         }
-#     }}],        
-#     }
+async def main():
+    class StateChangeCallback(EventCallbackInterface):
+        def handle_event(self, event_type: EventType, event_state: EventState):
+            assert event_state.event_type == EventType.Graph
+            triple = json.loads(event_state.payload)
+            print("triple: {}".format(triple))
+            assert isinstance(triple['subject'], str) and triple['subject']
+    config_source={
+    'version': 0.0,
+    'querent_id': '7b0e11f3-955c-4402-947d-78ec96d54131',
+    'querent_name': 'knowledge_graph_using_llama2_v1',
+    'workflow': {
+        'name': 'knowledge_graph_using_llama2_v1',
+        'id': '7b0e11f3-955c-4402-947d-78ec96d54131',
+        'config': {
+            
+        },
+        # 'channel': None,
+        'event_handler': StateChangeCallback,
+        'tokens_feader': None 
+    },
+    'collectors': [
+        {
+            'id': '7b0e11f3-955c-4402-947d-78ec96d54131',
+            'name': 'Drive-config',
+            'backend': 'drive',
+            'config': {
+                'drive_token': 'ya29.a0AfB_byAMnws17-UAYR2hU29zC83Rw4bxn2LsF5i_sWQ5xDMI00li205pXlA-JrwVmBh0kNBK7sKP33urPZ9-DM9DDKMv6EQsaqJsy57aHQYUwddT42SwuZAVINyTwp340Qiy_hSaVG5ezT9PIYRO5Qd1Yn9wm5rd7Aq-',
+                'chunk_size': 1048576,
+                'drive_refresh_token': '1//0g7Sd9WayGH-yCgYIARAAGBASNwF-L9Irh8XWYJ_zz43V0Ema-OqTCaHzdJKrNtgJDrrrRSs8z6iJU9dgR8tA1fucRKjwUVggwy8',
+                'drive_client_id': '4402204563-lso0f98dve9k33durfvqdt6dppl7iqn5.apps.googleusercontent.com',
+                'specific_file_type': 'application/pdf',
+                'folder_to_crawl': '1BtLKXcYBrS16CX0R4V1X7Y4XyO9Ct7f8',
+                'drive_client_secret': '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',
+                'drive_scopes': 'https://www.googleapis.com/auth/drive'
+            },
+            # 'channel': None,
+        }
+    ],
+    'engines': [
+        {
+            'id': '7b0e11f3-955c-4402-947d-78ec96d54131',
+            'name': 'knowledge_graph_using_llama2_v1',
+            'config': {
+                
+            },
+            # 'channel': None
+        }
+    ],
+    'resource': None
+    }
 
-#     await start_workflow(config_source)
+    await start_workflow(config_source)
     
-# asyncio.run(main())
+asyncio.run(main())
