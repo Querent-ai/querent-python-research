ca9f0708a3d56292f1753e46edd5e97bbaed8cda
ngupta10 <ngupta10.slb@gmail.com>
2024-01-30 11:23:53 +0530

    added a print to see config on querent side

--- querent/workflow/workflow.py
+++ querent/workflow/workflow.py
@@ -23,9 +23,9 @@ from querent.querent.resource_manager import ResourceManager
 
 
 
-async def start_workflow(config_dict: dict):
+async def start_workflow(config_dict: Any):
     #Start the workflow
-    # print("----------------------------------------------------------------Print the configuration :", config_dict)
+    print("----------------------------------------------------------------Print the configuration :", config_dict)
     workflow_config = config_dict.get("workflow")
     workflow = WorkflowConfig(config_source=workflow_config)
     collector_configs = config_dict.get("collectors", [])
@@ -116,7 +116,7 @@ async def start_llama_workflow(config: Config):
 
     llm_instance = BERTLLM(result_queue, config.engines[0])
 
-    llm_instance.subscribe(EventType.Graph, config.workflow.event_handler())
+    llm_instance.subscribe(EventType.Graph, config.workflow.event_handler.handle_event())
     querent = Querent(
         [llm_instance],
         resource_manager=resource_manager,
@@ -135,57 +135,57 @@ async def start_llama_workflow(config: Config):
 #             triple = json.loads(event_state.payload)
 #             print("triple: {}".format(triple))
 #             assert isinstance(triple['subject'], str) and triple['subject']
-#     config_source={
-#         "version": 1.0,
-#         "id": "ahdbfvd",
-#         "querent_id": 12345,
-#         "querent_name": "llama",
-#         "workflow": {
-#         "name": "llama",
-#         "id": str(uuid.uuid4()),
-#         "config": {},
-#         "event_handler": StateChangeCallback
-#     },
-#         "collectors": [{
-#             "id": str(uuid.uuid4()),
-#             "name": "Local-config",
-#             "config": {
-#                 "root_path": "./tests/data/llm/pdf",
-#             },
-#             "backend":"localfile",
-#         },
-#         {
-#             "id": str(uuid.uuid4()),
-#             "name": "Drive-config",
-#             "config": {
-#                 "drive_refresh_token": os.getenv("DRIVE_REFRESH_TOKEN"),
-#                 "drive_token": os.getenv("DRIVE_TOKEN"),
-#                 "drive_scopes": os.getenv("DRIVE_SCOPES"),
-#                 "chunk_size": 1024 * 1024,
-#                 "drive_client_id": os.getenv("DRIVE_CLIENT_ID"),
-#                 "drive_client_secret": os.getenv("DRIVE_CLIENT_SECRET"),
-#                 "specific_file_type": "application/pdf",
-#                 "folder_to_crawl": "1BtLKXcYBrS16CX0R4V1X7Y4XyO9Ct7f8",
-#             },
-#             "backend": "drive",
-#         }],
+    # config_source={
+    #     "version": 1.0,
+    #     "id": "ahdbfvd",
+    #     "querent_id": 12345,
+    #     "querent_name": "llama",
+    #     "workflow": {
+    #     "name": "llama",
+    #     "id": str(uuid.uuid4()),
+    #     "config": {},
+    #     "event_handler": StateChangeCallback
+    # },
+    #     "collectors": [{
+    #         "id": str(uuid.uuid4()),
+    #         "name": "Local-config",
+    #         "config": {
+    #             "root_path": "./tests/data/llm/pdf",
+    #         },
+    #         "backend":"localfile",
+    #     },
+    #     {
+    #         "id": str(uuid.uuid4()),
+    #         "name": "Drive-config",
+    #         "config": {
+    #             "drive_refresh_token": os.getenv("DRIVE_REFRESH_TOKEN"),
+    #             "drive_token": os.getenv("DRIVE_TOKEN"),
+    #             "drive_scopes": os.getenv("DRIVE_SCOPES"),
+    #             "chunk_size": 1024 * 1024,
+    #             "drive_client_id": os.getenv("DRIVE_CLIENT_ID"),
+    #             "drive_client_secret": os.getenv("DRIVE_CLIENT_SECRET"),
+    #             "specific_file_type": "application/pdf",
+    #             "folder_to_crawl": "1BtLKXcYBrS16CX0R4V1X7Y4XyO9Ct7f8",
+    #         },
+    #         "backend": "drive",
+    #     }],
         
-#         "engines": [{ 
-#         #https://github.com/Querent-ai/querent-rs/blob/main/src/config/config.rs#L172 con
-#         "id": str(uuid.uuid4()),
-#         "name": "knowledge_graph_using_llama2_v1",
-#         "config": {"ner_model_name":"botryan96/GeoBERT",
-#         "enable_filtering": True,
-#         "filter_params": {
-#                 'score_threshold': 0.5,
-#                 'attention_score_threshold': 0.1,
-#                 'similarity_threshold': 0.5,
-#                 'min_cluster_size': 5,
-#                 'min_samples': 3,
-#                 'cluster_persistence_threshold':0.1
-#         }
-#     }}],        
-#     }
+    #     "engines": [{ 
+    #     #https://github.com/Querent-ai/querent-rs/blob/main/src/config/config.rs#L172 con
+    #     "id": str(uuid.uuid4()),
+    #     "name": "knowledge_graph_using_llama2_v1",
+    #     "config": {"ner_model_name":"botryan96/GeoBERT",
+    #     "enable_filtering": True,
+    #     "filter_params": {
+    #             'score_threshold': 0.5,
+    #             'attention_score_threshold': 0.1,
+    #             'similarity_threshold': 0.5,
+    #             'min_cluster_size': 5,
+    #             'min_samples': 3,
+    #             'cluster_persistence_threshold':0.1
+    #     }
+    # }}],        
+    # }
 
 #     await start_workflow(config_source)
     
