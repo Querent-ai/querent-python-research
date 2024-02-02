b150f0053f20f30c88e6c481a5fdd10be1484300
Ansh5461 <anshjoshi0607@gmail.com>
2024-02-02 13:22:47 +0530

    Finalised workflow

--- querent/workflow/workflow.py
+++ querent/workflow/workflow.py
@@ -1,12 +1,5 @@
 """File to start workflow"""
 import asyncio
-import json
-from typing import Any
-import uuid
-import os
-from querent.callback.event_callback_interface import EventCallbackInterface
-from querent.channel.channel_interface import ChannelCommandInterface
-from querent.common.types.ingested_tokens import IngestedTokens
 from querent.config.config import Config
 from querent.config.workflow.workflow_config import WorkflowConfig
 from querent.config.collector.collector_config import CollectorConfig
@@ -17,11 +10,9 @@ from querent.common.uri import Uri
 from querent.ingestors.ingestor_manager import IngestorFactoryManager
 from querent.core.transformers.bert_llm import BERTLLM
 from querent.core.transformers.gpt_llm import GPTLLM
-from querent.common.types.querent_event import  EventState, EventType
+from querent.common.types.querent_event import EventType
 from querent.querent.querent import Querent
 from querent.querent.resource_manager import ResourceManager
-
-
 
 async def start_workflow(config_dict: dict):
     #Start the workflow
@@ -122,64 +113,5 @@ async def start_llama_workflow(config: Config):
         resource_manager=resource_manager,
     )
     querent_task = asyncio.create_task(querent.start())
-    # token_feeder = asyncio.create_task(receive_token_feeder(resource_manager=resource_manager, config=config, result_queue=result_queue, state_queue=llm_instance.state_queue))
-    await asyncio.gather(ingest_task, querent_task)
-    # , token_feeder) # Loop and do config.workflow.channel for termination event messageType = "stop"
-
-
-
-# async def main():
-#     class StateChangeCallback(EventCallbackInterface):
-#         def handle_event(self, event_type: EventType, event_state: EventState):
-#             assert event_state.event_type == EventType.Graph
-#             triple = json.loads(event_state.payload)
-#             print("triple: {}".format(triple))
-#             assert isinstance(triple['subject'], str) and triple['subject']
-#     config_source={
-#     'version': 0.0,
-#     'querent_id': '7b0e11f3-955c-4402-947d-78ec96d54131',
-#     'querent_name': 'knowledge_graph_using_llama2_v1',
-#     'workflow': {
-#         'name': 'knowledge_graph_using_llama2_v1',
-#         'id': '7b0e11f3-955c-4402-947d-78ec96d54131',
-#         'config': {
-            
-#         },
-#         # 'channel': None,
-#         'event_handler': StateChangeCallback,
-#         'tokens_feader': None 
-#     },
-#     'collectors': [
-#         {
-#             'id': '7b0e11f3-955c-4402-947d-78ec96d54131',
-#             'name': 'Drive-config',
-#             'backend': 'drive',
-#             'config': {
-#                 'drive_token': 'ya29.a0AfB_byAMnws17-UAYR2hU29zC83Rw4bxn2LsF5i_sWQ5xDMI00li205pXlA-JrwVmBh0kNBK7sKP33urPZ9-DM9DDKMv6EQsaqJsy57aHQYUwddT42SwuZAVINyTwp340Qiy_hSaVG5ezT9PIYRO5Qd1Yn9wm5rd7Aq-',
-#                 'chunk_size': 1048576,
-#                 'drive_refresh_token': '1//0g7Sd9WayGH-yCgYIARAAGBASNwF-L9Irh8XWYJ_zz43V0Ema-OqTCaHzdJKrNtgJDrrrRSs8z6iJU9dgR8tA1fucRKjwUVggwy8',
-#                 'drive_client_id': '4402204563-lso0f98dve9k33durfvqdt6dppl7iqn5.apps.googleusercontent.com',
-#                 'specific_file_type': 'application/pdf',
-#                 'folder_to_crawl': '1BtLKXcYBrS16CX0R4V1X7Y4XyO9Ct7f8',
-#                 'drive_client_secret': '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',
-#                 'drive_scopes': 'https://www.googleapis.com/auth/drive'
-#             },
-#             # 'channel': None,
-#         }
-#     ],
-#     'engines': [
-#         {
-#             'id': '7b0e11f3-955c-4402-947d-78ec96d54131',
-#             'name': 'knowledge_graph_using_llama2_v1',
-#             'config': {
-                
-#             },
-#             # 'channel': None
-#         }
-#     ],
-#     'resource': None
-#     }
-
-#     await start_workflow(config_source)
-    
-# asyncio.run(main())
+    token_feeder = asyncio.create_task(receive_token_feeder(resource_manager=resource_manager, config=config, result_queue=result_queue, state_queue=llm_instance.state_queue))
+    await asyncio.gather(ingest_task, querent_task, token_feeder) # Loop and do config.workflow.channel for termination event messageType = "stop"
