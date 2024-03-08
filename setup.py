cd8bebd004d5fc01a14827e375b8b2fb6ecc88b2
Ansh5461 <anshjoshi0607@gmail.com>
2024-03-08 12:53:05 +0530

    Changed versions

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="querent",
-    version="1.1.1",
+    version="1.1.2",
     author="Querent AI",
     description="The Asynchronous Data Dynamo and Graph Neural Network Catalyst",
     long_description=long_description,
