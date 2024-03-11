e81a485166b745e2f9e65f797e2945f766a32ea3
Ansh5461 <anshjoshi0607@gmail.com>
2024-03-11 14:15:59 +0530

    Commented out code

--- querent/storage/milvus_delete_collections.py
+++ querent/storage/milvus_delete_collections.py
@@ -1,12 +1,12 @@
-from pymilvus import utility
+# from pymilvus import utility
 
-from pymilvus import connections, utility
+# from pymilvus import connections, utility
 
-# Connect to the Milvus server
-connections.connect("default", host="localhost", port="19530")
+# # Connect to the Milvus server
+# connections.connect("default", host="localhost", port="19530")
 
-# List all collections
-collections = utility.list_collections()
+# # List all collections
+# collections = utility.list_collections()
 
-for collection_name in collections:
-    utility.drop_collection(collection_name=collection_name)
+# for collection_name in collections:
+#     utility.drop_collection(collection_name=collection_name)
