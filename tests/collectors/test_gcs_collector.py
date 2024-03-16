a1381491dba5cf7dd3ee4bd97a040fd9ab4d331c
Ansh5461 <anshjoshi0607@gmail.com>
2024-03-16 15:14:37 +0530

    tested gcs

--- tests/collectors/test_gcs_collector.py
+++ tests/collectors/test_gcs_collector.py
@@ -9,12 +9,25 @@ import uuid
 from dotenv import load_dotenv
 
 load_dotenv()
+import os
 
 
 @pytest.fixture
 def gcs_config():
     cred_file = "/tmp/.config/gcloud/application_default_credentials.json"
-    credentials_info = json.load(open(cred_file))
+    credentials_info = {
+        "type": os.getenv("GOOGLE_BUCKET_TYPE"),
+        "project_id": os.getenv("GOOGLE_BUCKET_PROJECT_ID"),
+        "private_key_id": os.getenv("GOOGLE_BUCKET_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("GOOGLE_BUCKET_PRIVATE_KEY"),
+        "client_email": os.getenv("GOOGLE_BUCKET_CLIENT_EMAIL"),
+        "client_id": os.getenv("GOOGLE_BUCKET_CLIENT_ID"),
+        "auth_uri": os.getenv("GOOGLE_BUCKET_AUTH_URI"),
+        "token_uri": os.getenv("GOOGLE_BUCKET_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv("GOOGLE_BUCKET_AUTH_PROVIDER"),
+        "client_x509_cert_url": os.getenv("GOOGLE_BUCKET_CLIENT_CERT_URL"),
+        "universe_domain": os.getenv("GOOGLE_BUCKET_UNIVERSE_DOMAIN")
+    }
     credential_json_str = json.dumps(credentials_info)
     return GcsCollectConfig(
         config_source={
