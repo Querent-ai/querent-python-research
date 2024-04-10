ec0059bb14ed148e031df7ba9e27200a1e5b7d4c
Ansh5461 <anshjoshi0607@gmail.com>
2024-04-10 14:54:33 +0530

    Changed assertion

--- tests/workflows/test_async_collectors_ingestion.py
+++ tests/workflows/test_async_collectors_ingestion.py
@@ -51,7 +51,7 @@ async def test_ingest_all_async():
         if ingested_data is not None:
             unique_files.add(ingested_data.file)
             counter += 1
-    assert counter == 95
+    assert counter == 102
     assert len(unique_files) == 5
 
 
