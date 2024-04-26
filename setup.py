2537a9f5b1f27a76e33a6f9703d400a96e07cabd
ngupta10 <ngupta10.slb@gmail.com>
2024-04-26 23:55:23 +0530

    updated requirements

--- setup.py
+++ setup.py
@@ -67,7 +67,10 @@ requirements = [
     "spacy==3.7.2",
     "speechrecognition==3.10.1",
     "tika==2.6.0",
-    "tensorflow==2.14.0"
+    "tensorflow==2.14.0",
+    "torch==2.3.0+cpu",
+    "torchaudio==2.3.0+cpu",
+    "torchvision==0.18.0+cpu",
     "transformers==4.36.0",
     "unidecode==1.3.7",
 ]
